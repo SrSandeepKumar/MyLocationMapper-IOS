fff3efb4519bbdba5a30e55f171ff0fbf1aec9e4
Native5 <native5@mac01.native5.private>
2014-10-09 16:08:41 +0530

    Added untracked files

--- platforms/ios/www/cordova_plugins.js
+++ platforms/ios/www/cordova_plugins.js
@@ -1,13 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/plugin.google.maps/www/googlemaps-cdv-plugin.js",
-        "id": "plugin.google.maps.phonegap-googlemaps-plugin",
-        "clobbers": [
-            "plugin.google.maps"
-        ]
-    },
-    {
         "file": "plugins/nl.x-services.plugins.toast/www/Toast.js",
         "id": "nl.x-services.plugins.toast.Toast",
         "clobbers": [
@@ -24,37 +17,42 @@ module.exports = [
         "merges": [
             "navigator.notification"
         ]
-                  },{
-                  "file": "plugins/com.native5.plugins.navbar/www/NavigationBar.js",
-                  "id": "com.native5.plugins.navbar.navbar",
-                  "clobbers": [
-                               "window.navbar"
-                               ]
-                  },
-                  {
-                  "file": "plugins/ru.dextra.cordova.tabbar/www/TabBar.js",
-                  "id": "ru.dextra.cordova.tabbar.tabbar",
-                  "clobbers": [
-                               "window.tabbar"
-                               ]
-                  },
-
+    },
     {
-        "file": "plugins/plugin.http.request/www/http-request.js",
-        "id": "plugin.http.request.phonegap-http-requst",
+        "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
+        "id": "org.apache.cordova.geolocation.Coordinates",
         "clobbers": [
-            "cordova.plugins.http-request"
+            "Coordinates"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/PositionError.js",
+        "id": "org.apache.cordova.geolocation.PositionError",
+        "clobbers": [
+            "PositionError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/Position.js",
+        "id": "org.apache.cordova.geolocation.Position",
+        "clobbers": [
+            "Position"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.geolocation/www/geolocation.js",
+        "id": "org.apache.cordova.geolocation.geolocation",
+        "clobbers": [
+            "navigator.geolocation"
         ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "plugin.google.maps": "1.1.4",
     "nl.x-services.plugins.toast": "2.0.1",
     "org.apache.cordova.dialogs": "0.2.10-dev",
-    "plugin.http.request": "1.0.0",
-    "com.googlemaps.ios": "1.8.1"
+    "org.apache.cordova.geolocation": "0.3.11-dev"
 }
 // BOTTOM OF METADATA
 });
